8f0ffe77763eae8a94bb7a161a3c10dbe3556400
Alexey Smirnov <alexey@gmail.com>
2017-10-05 17:42:29 +0800

    Initial Commit

---
+++ swift-toolkit/swift_toolkit.h
@@ -0,0 +1,19 @@
+//
+//  swift_toolkit.h
+//  swift-toolkit
+//
+//  Created by Alexey Smirnov on 10/5/17.
+//  Copyright © 2017 Alexey Smirnov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swift_toolkit.
+FOUNDATION_EXPORT double swift_toolkitVersionNumber;
+
+//! Project version string for swift_toolkit.
+FOUNDATION_EXPORT const unsigned char swift_toolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_toolkit/PublicHeader.h>
+
+
