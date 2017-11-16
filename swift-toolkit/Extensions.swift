84e95bfc1dc7cfbf0b4bce81492e8d145f9f1f87
Alexey Smirnov <alexey@gmail.com>
2017-11-16 18:21:16 +0800

    clear color

--- swift-toolkit/Extensions.swift
+++ swift-toolkit/Extensions.swift
@@ -224,23 +224,28 @@ public func >> (left: Date, right: Date) -> Int {
 // http://stackoverflow.com/a/29218836/995049
 public extension UIColor {
     convenience init(hex: String) {
-        let alpha: Float = 100
-        
-        // Establishing the rgb color
-        var rgb: UInt32 = 0
-        let s: Scanner = Scanner(string: hex)
-        // Setting the scan location to ignore the leading `#`
-        s.scanLocation = 1
-        // Scanning the int into the rgb colors
-        s.scanHexInt32(&rgb)
-        
-        // Creating the UIColor from hex int
-        self.init(
-            red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgb & 0x0000FF) / 255.0,
-            alpha: CGFloat(alpha / 100)
-        )
+        if hex == "" {
+            self.init(red: 0, green: 0, blue: 0, alpha: 0)
+            
+        } else {
+            let alpha: Float = 100
+            
+            // Establishing the rgb color
+            var rgb: UInt32 = 0
+            let s: Scanner = Scanner(string: hex)
+            // Setting the scan location to ignore the leading `#`
+            s.scanLocation = 1
+            // Scanning the int into the rgb colors
+            s.scanHexInt32(&rgb)
+            
+            // Creating the UIColor from hex int
+            self.init(
+                red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgb & 0x0000FF) / 255.0,
+                alpha: CGFloat(alpha / 100)
+            )
+        }
     }
 }
 
