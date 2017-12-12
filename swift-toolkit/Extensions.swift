1adc57eb72cf263351115d1df366ab9d6067e4de
Alexey Smirnov <alexey@gmail.com>
2017-12-12 12:08:54 +0800

    debug

--- swift-toolkit/Extensions.swift
+++ swift-toolkit/Extensions.swift
@@ -417,6 +417,35 @@ public extension UIImage {
     }
 }
 
+extension UIImageView{
+    func frameForImageInImageViewAspectFit() -> CGRect
+    {
+        if  let img = self.image {
+            let imageRatio = img.size.width / img.size.height;
+            let viewRatio = self.frame.size.width / self.frame.size.height;
+            
+            if(imageRatio < viewRatio)
+            {
+                let scale = self.frame.size.height / img.size.height;
+                let width = scale * img.size.width;
+                let topLeftX = (self.frame.size.width - width) * 0.5;
+                
+                return CGRect(x: topLeftX, y: 0, width: width, height: self.frame.size.height)
+            }
+            else
+            {
+                let scale = self.frame.size.width / img.size.width;
+                let height = scale * img.size.height;
+                let topLeftY = (self.frame.size.height - height) * 0.5;
+                
+                return CGRect(x: 0, y: topLeftY, width: self.frame.size.width, height: height)
+            }
+        }
+        
+        return CGRect(x: 0, y: 0, width: 0, height: 0)
+    }
+}
+
 public extension UIImageView {
     func downloadedFrom(link:String, contentMode mode: UIViewContentMode, cell: UIView) {
         guard let url = URL(string: link) else { return }
@@ -458,6 +487,9 @@ public extension UIImageView {
             
             DispatchQueue.main.async { () -> Void in
                 self.image = image
+                
+                let rect = self.frameForImageInImageViewAspectFit()
+                self.frame = rect
                 
                 cell.setNeedsLayout()
                 cell.setNeedsUpdateConstraints()
