d7b49bda605df61fa7292cd6db0c8f02675cd1f2
IY0018 <alexey@ironyun.com>
2018-11-16 15:23:16 +0800

    added WrapperView

--- swift-toolkit/ViewControllerAnimated.swift
+++ swift-toolkit/ViewControllerAnimated.swift
@@ -119,7 +119,6 @@ public extension ResizableTableViewCells where Self: UIViewController {
     }
     
     public func calculateHeightForCell(_ cell: UITableViewCell) -> CGFloat {
-        // 25 for margins
         cell.bounds = CGRect(x: 0, y: 0, width: tableView.frame.width-1, height: cell.bounds.height)
         cell.setNeedsLayout()
         cell.layoutIfNeeded()
