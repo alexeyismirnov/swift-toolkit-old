06aefae2fe496661953c4b5d9ed1f3eafefc97cc
IY0018 <alexey@ironyun.com>
2019-04-07 16:59:23 +0800

    toHexString

--- swift-toolkit/Extensions.swift
+++ swift-toolkit/Extensions.swift
@@ -268,6 +268,19 @@ public extension UIColor {
             return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
         }
     }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        
+        return String(format:"#%06x", rgb)
+    }
 }
 
 public extension UIImage {
