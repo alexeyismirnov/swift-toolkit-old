210383b8d27cc9260b5a39fb1489ccd64e3bba86
IY0018 <alexey@ironyun.com>
2019-05-27 12:06:27 +0800

    NSAttributedString.font

--- swift-toolkit/SwifterSwift.swift
+++ swift-toolkit/SwifterSwift.swift
@@ -9,29 +9,33 @@
 import Foundation
 
 public extension String {
-    public func colored(with color: UIColor) -> NSAttributedString {
+    func colored(with color: UIColor) -> NSAttributedString {
         return NSAttributedString(string: self).colored(with: color)
     }
 }
 
 public extension NSAttributedString {
     
-    public var centered: NSAttributedString {
+    var centered: NSAttributedString {
         let centerStyle = NSMutableParagraphStyle()
         centerStyle.alignment = .center
         
         return applying(attributes: [.paragraphStyle: centerStyle])
     }
     
-    public func colored(with color: UIColor) -> NSAttributedString {
+    func colored(with color: UIColor) -> NSAttributedString {
         return applying(attributes: [.foregroundColor: color])
     }
     
-    public func systemFont(ofSize: CGFloat) -> NSAttributedString {
+    func font(font: UIFont) -> NSAttributedString {
+        return applying(attributes: [.font: font])
+    }
+    
+    func systemFont(ofSize: CGFloat) -> NSAttributedString {
         return applying(attributes: [.font: UIFont.systemFont(ofSize: CGFloat(ofSize))])
     }
     
-    public func boldFont(ofSize: CGFloat) -> NSAttributedString {
+    func boldFont(ofSize: CGFloat) -> NSAttributedString {
         return applying(attributes: [.font: UIFont.boldSystemFont(ofSize: CGFloat(ofSize))])
     }
     
@@ -43,23 +47,23 @@ public extension NSAttributedString {
         return copy
     }
     
-    public static func += (lhs: inout NSAttributedString, rhs: NSAttributedString) {
+    static func += (lhs: inout NSAttributedString, rhs: NSAttributedString) {
         let string = NSMutableAttributedString(attributedString: lhs)
         string.append(rhs)
         lhs = string
     }
     
-    public static func + (lhs: NSAttributedString, rhs: NSAttributedString) -> NSAttributedString {
+    static func + (lhs: NSAttributedString, rhs: NSAttributedString) -> NSAttributedString {
         let string = NSMutableAttributedString(attributedString: lhs)
         string.append(rhs)
         return NSAttributedString(attributedString: string)
     }
     
-    public static func += (lhs: inout NSAttributedString, rhs: String) {
+    static func += (lhs: inout NSAttributedString, rhs: String) {
         lhs += NSAttributedString(string: rhs)
     }
     
-    public static func + (lhs: NSAttributedString, rhs: String) -> NSAttributedString {
+    static func + (lhs: NSAttributedString, rhs: String) -> NSAttributedString {
         return lhs + NSAttributedString(string: rhs)
     }
     
