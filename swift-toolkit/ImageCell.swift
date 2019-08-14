bd43ea28a835f1287f9dfa166a724e246c672bcf
IY0018 <alexey@ironyun.com>
2019-08-14 17:05:51 +0800

    ImageCell refactored

--- swift-toolkit/ImageCell.swift
+++ swift-toolkit/ImageCell.swift
@@ -9,8 +9,46 @@
 import UIKit
 
 public class ImageCell : UITableViewCell {
-    @IBOutlet public weak var title: RWLabel!
-    @IBOutlet public weak var icon: UIImageView!
+    public var title: RWLabel!
+    public var icon: UIImageView!
+    
+    var con : [NSLayoutConstraint]!
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        title = RWLabel()
+        title.numberOfLines = 0
+        title.preferredMaxLayoutWidth = 310
+        
+        title.font = UIFont.systemFont(ofSize: 17.0)
+        title.adjustsFontSizeToFitWidth = false
+        title.translatesAutoresizingMaskIntoConstraints = false
+        
+        icon = UIImageView()
+        
+        contentView.addSubview(title)
+        contentView.addSubview(icon)
+        
+        con = [
+            icon.widthAnchor.constraint(equalToConstant: 35.0),
+            icon.heightAnchor.constraint(equalToConstant: 35.0),
+            icon.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            icon.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20.0),
+            icon.rightAnchor.constraint(equalTo: title.leftAnchor, constant: -10.0),
+
+            title.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            title.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20.0),
+
+        ]
+        
+        NSLayoutConstraint.activate(con)
+    }
+    
 }
 
 extension ImageCell: ReusableView {}
