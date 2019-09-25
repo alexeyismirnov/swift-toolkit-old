514f5da5ceb3271d9effe6545d011332da5e2693
IY0018 <alexey@ironyun.com>
2019-09-25 15:34:29 +0800

    startDate

--- swift-toolkit/ServiceDateSelector.swift
+++ swift-toolkit/ServiceDateSelector.swift
@@ -22,7 +22,8 @@ public class ServiceDateSelector: UIViewController, ResizableTableViewCells, Pop
     
     public init?(_ model: BookModel) {
         self.model = model
-        self.iterator = model.dateIterator(startDate: DateComponents(date: Date()).toDate())
+//        self.iterator = model.dateIterator(startDate: DateComponents(date: Date()).toDate())
+        self.iterator = model.dateIterator(startDate: Date(2019,2,17))
         super.init(nibName: nil, bundle: nil)
     }
     
