c2dae7e3c93ceb2b3ce73750683f9b6d66647368
alexeyismirnov <alexey@gmail.com>
2021-08-09 17:58:58 +0800

    inc fontSize for PonomarUnicode

--- swift-toolkit/Extensions.swift
+++ swift-toolkit/Extensions.swift
@@ -316,7 +316,7 @@ public extension UIFont {
             self.init(name: "STHeitiSC-Light", size: CGFloat(fontSize))!
             
         } else if lang == "cs" {
-            self.init(name: "PonomarUnicode", size: CGFloat(fontSize))!
+            self.init(name: "PonomarUnicode", size: CGFloat(fontSize+2))!
             
         } else {
             self.init(name: "HelveticaNeue", size: CGFloat(fontSize))!
