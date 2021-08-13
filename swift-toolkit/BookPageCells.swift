c2c2b58c2d96fcf8c38c3e32a92963d18f94d29f
alexeyismirnov <alexey@gmail.com>
2021-08-13 22:00:58 +0800

    debug

--- swift-toolkit/BookPageCells.swift
+++ swift-toolkit/BookPageCells.swift
@@ -30,6 +30,7 @@ class BookPageCellText: UICollectionViewCell, UITextViewDelegate {
     func updateText() {
         textView.attributedText = attributedText
         textView.font = font
+        textView.textColor = Theme.textColor
         textView.setContentOffset(.zero, animated: false)
         textView.scrollRangeToVisible(NSRange(location:0, length:0))
 
